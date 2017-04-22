d5e2f19464401ae130a4263774282e7c26881960
Bojan Rajkovic <bojan.rajkovic@microsoft.com>
2017-04-21 21:02:43 -0400

    Oxide.Http: Add a small library of HTTP helper routines.
    Oxide.Http provides a small set of wrappers around HttpClient that safely
    invoke the various Get*Async methods by wrapping them in Result<T, Exception>
    rather than forcing the caller to deal with exception handling.

---
+++ src/Oxide.Http/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Oxide.Tests")]
+
+[assembly: AssemblyCompany("Code, Rinse, Repeat")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
