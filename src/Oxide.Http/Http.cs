2ef94cbb9d9da4fa6e79e885a8f4d48c35cc6554
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-04-21 21:02:43 -0400

    Oxide.Http: Add a small library of HTTP helper routines.
    Oxide.Http provides a small set of wrappers around HttpClient that safely
    invoke the various Get*Async methods by wrapping them in Result<T, Exception>
    rather than forcing the caller to deal with exception handling.

---
+++ src/Oxide.Http/Http.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Oxide
+{
+    public static class Http
+    {
+        public static async Task<Result<string, Exception>> SafelyGetStringAsync(
+            this HttpClient client,
+            string requestUri
+        )
+        {
+            try {
+                var result = await client.GetAsync(requestUri);
+                result.EnsureSuccessStatusCode();
+                return await result.Content.ReadAsStringAsync();
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<Stream, Exception>> SafelyGetStreamAsync(
+            this HttpClient client,
+            string requestUri
+        )
+        {
+            try {
+                var result = await client.GetAsync(requestUri);
+                result.EnsureSuccessStatusCode();
+                return await result.Content.ReadAsStreamAsync();
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<byte[], Exception>> SafelyGetByteArrayAsync(
+            this HttpClient client,
+            string requestUri
+        )
+        {
+            try {
+                var result = await client.GetAsync(requestUri);
+                result.EnsureSuccessStatusCode();
+                return await result.Content.ReadAsByteArrayAsync();
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<HttpResponseMessage, Exception>> SafelyGetAsync(
+            this HttpClient client,
+            string requestUri
+        )
+        {
+            try {
+                var result = await client.GetAsync(requestUri);
+                result.EnsureSuccessStatusCode();
+                return result;
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<HttpResponseMessage, Exception>> SafelyPostAsync(
+            this HttpClient client,
+            string requestUri,
+            HttpContent content
+        )
+        {
+            try {
+                var result = await client.PostAsync(requestUri, content);
+                result.EnsureSuccessStatusCode();
+                return result;
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<HttpResponseMessage, Exception>> SafelyPutAsync(
+            this HttpClient client,
+            string requestUri,
+            HttpContent content
+        )
+        {
+            try {
+                var result = await client.PutAsync(requestUri, content);
+                result.EnsureSuccessStatusCode();
+                return result;
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        public static async Task<Result<HttpResponseMessage, Exception>> SafelyDeleteAsync(
+            this HttpClient client,
+            string requestUri
+        )
+        {
+            try {
+                var result = await client.DeleteAsync(requestUri);
+                result.EnsureSuccessStatusCode();
+                return result;
+            } catch (Exception e) {
+                return e;
+            }
+        }
+    }
+}
