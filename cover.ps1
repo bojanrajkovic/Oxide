b0640122c58029f74ea50b52460865b107bb34a2
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-05-06 14:47:25 -0400

    Coverage: Remove threshold:1 from script.
    I snagged the script from bojanrajkovic/Pingu, and Pingu needed it because if
    the coverage counted how many times each line was hit, the tests would take
    forever to run, because of all the tight unrolled loops in Pingu. Oxide has no
    such loops.

--- cover.ps1
+++ cover.ps1
@@ -4,7 +4,7 @@ nuget install -OutputDirectory packages -Version 2.5.1 ReportGenerator
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
 	-register:user -target:"C:\Program Files\dotnet\dotnet.exe" `
 	-targetargs:"test src\Oxide.Tests\Oxide.Tests.csproj" -output:"coverage.xml" -oldstyle `
-	-filter:+[*]* -filter:-[Oxide.Tests]* -threshold:1
+	-filter:+[*]* -filter:-[Oxide.Tests]*
 
 .\packages\ReportGenerator.2.5.1\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:coverage
 
