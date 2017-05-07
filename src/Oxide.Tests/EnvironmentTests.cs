42e6f8f77058815dd7a58539e4503e79ff5fb9a0
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-05-06 23:15:37 -0400

    Oxide: Add fetching for environment variables that returns Option<T>
    Splits support between .NET Standard 1.3 and .NET Standard 1.0. The 1.0 build
    doesn't have the Environment helpers, as System.Environment is not available in
    .NET Standard 1.0. Multi-targeting makes this really easy. :D

---
+++ src/Oxide.Tests/EnvironmentTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Xunit;
+
+using SEnvironment = System.Environment;
+
+namespace Oxide.Tests
+{
+    public class EnvironmentTests
+    {
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        [Theory]
+        public void Passing_null_or_whitespace_returns_none(string environmentVariable)
+            => Assert.True(Environment.GetEnvironmentVariable(environmentVariable).IsNone);
+
+        [Fact]
+        public void Set_environment_variable_returns_value()
+        {
+            const string environmentVariable = "OXIDE_TEST_ENVIRONMENT_VARIABLE";
+            var randomValue = DateTime.UtcNow.ToString("o");
+
+            SEnvironment.SetEnvironmentVariable(environmentVariable, randomValue);
+
+            var fetchedEnv = Environment.GetEnvironmentVariable(environmentVariable);
+
+            Assert.True(fetchedEnv.IsSome);
+            Assert.Equal(randomValue, fetchedEnv.Unwrap());
+        }
+
+        [Fact]
+        public void Unset_environment_variable_returns_none()
+            => Assert.True(Environment.GetEnvironmentVariable("OXIDE_TEST_UNSET_VARIABLE").IsNone);
+    }
+}
---
+++ src/Oxide/Environment.cs
@@ -0,0 +1,24 @@
+﻿using static Oxide.Options;
+
+#if NETSTANDARD1_3
+
+namespace Oxide
+{
+    class Environment
+    {
+        public static Option<string> GetEnvironmentVariable(string environmentVariable)
+        {
+            if (string.IsNullOrWhiteSpace(environmentVariable))
+                return None<string>();
+
+            try {
+                var value = System.Environment.GetEnvironmentVariable(environmentVariable);
+                return string.IsNullOrWhiteSpace(value) ? None<string>() : Some(value);
+            } catch {
+                return None<string>();
+            }
+        }
+    }
+}
+
+#endif
