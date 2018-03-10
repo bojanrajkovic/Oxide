458961b387f7a2cc981016c2e33a0ea0e4ed136c
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2018-03-10 14:08:28 -0500

    Oxide: Switch to versioning via GitVersion.
    Be more compliant with SemVer--instead of incrementing on every commit
    (this makes sense for continuous deployment apps, not for libraries),
    increment manually via tags, but preserve commit distance, branch, etc.
    in metadata in the assembly.

--- src/Oxide/AssemblyInfo.cs
+++ src/Oxide/AssemblyInfo.cs
@@ -2,24 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Oxide.Tests")]
-
 [assembly: AssemblyCompany("Code, Rinse, Repeat")]
-
-[assembly: AssemblyVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor
-)]
-
-[assembly: AssemblyFileVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch
-)]
-
-[assembly: AssemblyInformationalVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch + "-" +
-    ThisAssembly.Git.Branch + "+" +
-    ThisAssembly.Git.Commit
-)]
