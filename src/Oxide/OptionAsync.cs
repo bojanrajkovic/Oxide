12322da71fdb1890e8a2652022bab7bafee1b302
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2019-11-25 22:17:22 -0500

    Option: Reformat OptionAwaiter.

--- src/Oxide/OptionAsync.cs
+++ src/Oxide/OptionAsync.cs
@@ -1,74 +1,78 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-
 using static Oxide.Options;
 
 namespace Oxide
 {
-	public struct OptionAsyncMethodBuilder<T>
-	{
-		Option<T> task;
+    public struct OptionAsyncMethodBuilder<T>
+    {
+        Option<T> task;
 
-		OptionAsyncMethodBuilder(Option<T> initialValue)
-		{
-			task = initialValue;
-		}
+        OptionAsyncMethodBuilder(Option<T> initialValue)
+        {
+            task = initialValue;
+        }
 
-		public static OptionAsyncMethodBuilder<T> Create() => new OptionAsyncMethodBuilder<T>(None<T>());
+        public static OptionAsyncMethodBuilder<T> Create() => new OptionAsyncMethodBuilder<T>(None<T>());
 
-		public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-		{
-			// Simply start the state machine which will execute our code
-			stateMachine.MoveNext();
-		}
+        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
+        {
+            // Simply start the state machine which will execute our code
+            stateMachine.MoveNext();
+        }
 
-		public Option<T> Task => task;
+        public Option<T> Task => task;
 
-		public void SetStateMachine(IAsyncStateMachine stateMachine) { }
-		public void SetResult(T result) => task = Some(result);
-		public void SetException(Exception ex) { /* We leave the result to None */ }
+        public void SetStateMachine(IAsyncStateMachine stateMachine) { }
+        public void SetResult(T result) => task = Some(result);
 
-		public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
-				where TAwaiter : INotifyCompletion
-				where TStateMachine : IAsyncStateMachine
-		{
-			throw new NotSupportedException();
-		}
+        public void SetException(Exception ex)
+        {
+            /* We leave the result to None */
+        }
 
-		public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
-				where TAwaiter : ICriticalNotifyCompletion
-				where TStateMachine : IAsyncStateMachine
-		{
-			throw new NotSupportedException();
-		}
-	}
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : INotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            throw new NotSupportedException();
+        }
 
-	public struct OptionAwaiter<T> : INotifyCompletion
-	{
-		Option<T> option;
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter,
+            ref TStateMachine stateMachine)
+            where TAwaiter : ICriticalNotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            throw new NotSupportedException();
+        }
+    }
 
-		public OptionAwaiter(Option<T> option)
-		{
-			this.option = option;
-		}
+    public struct OptionAwaiter<T> : INotifyCompletion
+    {
+        Option<T> option;
 
-		public bool IsCompleted => option.IsSome;
+        public OptionAwaiter(Option<T> option)
+        {
+            this.option = option;
+        }
 
-		public void OnCompleted(Action continuation)
-		{
-			/* We never need to execute the continuation cause
-			 * we only reach here when the result is None which
-			 * means we are trying to short-circuit everything
-			 * else
-			 */
-		}
+        public bool IsCompleted => option.IsSome;
 
-		public T GetResult() => ((Some<T>)option).Unwrap();
-	}
+        public void OnCompleted(Action continuation)
+        {
+            /* We never need to execute the continuation cause
+             * we only reach here when the result is None which
+             * means we are trying to short-circuit everything
+             * else
+             */
+        }
 
-	public static class OptionAsyncExtensions
-	{
-		public static OptionAwaiter<T> GetAwaiter<T>(this Option<T> option)
-			=> new OptionAwaiter<T>(option);
-	}
+        public T GetResult() => ((Some<T>)option).Unwrap();
+    }
+
+    public static class OptionAsyncExtensions
+    {
+        public static OptionAwaiter<T> GetAwaiter<T>(this Option<T> option)
+            => new OptionAwaiter<T>(option);
+    }
 }
